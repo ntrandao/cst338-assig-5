2bf355effc26d2f2cebb96ad8f6e28a95b802bbb
Ericka Koyama <ericka.koyama@gmail.com>
2020-04-03 19:14:10 -0700

    initial commit

---
+++ src/com/company/Assig5PhaseOne.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Assig5PhaseOne {
+
+    public static void main(String[] args) {
+	     // write your code here
+    }
+}
---
+++ src/com/company/Assig5PhaseTwo.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Assig5PhaseTwo {
+
+   public static void main(String[] args) {
+      // write your code here
+   }
+}
