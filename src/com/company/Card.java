33562b6c0540f275731f67eccd260c72efe12095
Ericka Koyama <ericka.koyama@gmail.com>
2020-04-03 19:25:08 -0700

    Add Card, Hand, and Deck class

---
+++ src/com/company/Card.java
@@ -0,0 +1,136 @@
+package com.company;
+
+/**
+ * Card class represents a single card.
+ */
+public class Card {
+   public enum Suit { // All possible suits of a card
+      CLUBS,
+      DIAMONDS,
+      HEARTS,
+      SPADES
+   }
+
+   private char value;
+   private Suit suit;
+   private boolean errorFlag; // Whether this Card has an error.
+
+   /**
+    * No argument constructor
+    */
+   Card() {
+      value = 'A';
+      suit = Suit.SPADES;
+   }
+
+   /**
+    * Constructor with value and suit.
+    *
+    * @param value The value of the card.
+    * @param suit  The suit of the card.
+    */
+   Card(char value, Suit suit) {
+      set(value, suit);
+   }
+
+   /**
+    * Copy constructor
+    *
+    * @param card Card to make copy of.
+    */
+   Card(Card card) {
+      set(card.getValue(), card.getSuit());
+   }
+
+   /**
+    * Check if the given Card object equals this one.
+    *
+    * @param card Card instance to check for equality to this.
+    * @return Whether the Card objects are equal.
+    */
+   public boolean equals(Card card) {
+      return card != null && card.getSuit() == suit && card.getValue() == value;
+   }
+
+   /**
+    * Check if this Card has an error.
+    *
+    * @return Whether the Card object has an error.
+    */
+   public boolean getErrorFlag() {
+      return errorFlag;
+   }
+
+   /**
+    * Get the suit of this card.
+    *
+    * @return Suit of card.
+    */
+   public Suit getSuit() {
+      return suit;
+   }
+
+   /**
+    * Get the value of this card.
+    *
+    * @return Value of this card.
+    */
+   public char getValue() {
+      return value;
+   }
+
+   /**
+    * Check if the parameters given are valid for a Card.
+    *
+    * @param value Value given.
+    * @param suit  Suit given.
+    * @return Whether the parameters are valid.
+    */
+   private boolean isValid(char value, Suit suit) {
+      if (value >= '2' && value <= '9') {
+         return true;
+      }
+
+      switch (value) {
+         case 'T':
+         case 'J':
+         case 'Q':
+         case 'K':
+         case 'A':
+            return true;
+         default:
+            return false;
+      }
+   }
+
+   /**
+    * Set given parameters for Card.
+    *
+    * @param value Value to set for Card.
+    * @param suit  Suit to set for Card.
+    * @return Whether the parameters were able to be set.
+    */
+   public boolean set(char value, Suit suit) {
+      errorFlag = !isValid(value, suit);
+
+      if (!errorFlag) {
+         this.value = value;
+         this.suit = suit;
+      }
+
+      return !errorFlag;
+   }
+
+   /**
+    * Display Card as a String.
+    *
+    * @return Card data as a String.
+    */
+   public String toString() {
+      if (!errorFlag) {
+         return value + " of " + suit;
+      } else {
+         return "** illegal **";
+      }
+   }
+}
---
+++ src/com/company/Deck.java
@@ -0,0 +1,114 @@
+package com.company;
+
+/**
+ * Deck class represents a deck of cards.
+ */
+public class Deck {
+   public static final int MAX_CARDS = 312; // 6*52 packs of 52 cards
+   private static int PACK_SIZE = 52;
+   private static Card[] masterPack = new Card[PACK_SIZE];
+   private Card[] cards;
+   private int topCard; // 0 when the deck is empty
+
+   /**
+    * No argument constructor
+    */
+   Deck() {
+      this(1); // default size of 1 pack
+   }
+
+   /**
+    * Constructor with number of packs.
+    *
+    * @param numPacks Number of packs to include in deck.
+    */
+   Deck(int numPacks) {
+      topCard = 0;
+      cards = new Card[MAX_CARDS];
+
+      allocateMasterPack();
+      init(numPacks);
+   }
+
+   /**
+    * Fill up the deck with the given number of card packs.
+    *
+    * @param numPacks Number of packs to initialize the deck.
+    */
+   public void init(int numPacks) {
+      // fill up cards array
+      topCard = numPacks * PACK_SIZE;
+      for (int i = 0; i < numPacks; i++) {
+         for (int j = 0; j < PACK_SIZE; j++) {
+            cards[(PACK_SIZE * i) + j] = new Card(masterPack[j]);
+         }
+      }
+   }
+
+   /**
+    * Shuffle the deck in a random order.
+    */
+   public void shuffle() {
+      // mixes up the cards with the help of the standard random number generator.
+      for (int i = 0; i < topCard; i++) {
+         int index = (int) (Math.random() * (PACK_SIZE - 1));
+         Card temp = cards[index];
+         cards[index] = cards[i];
+         cards[i] = temp;
+      }
+   }
+
+   /**
+    * Deal out a Card from cards.
+    *
+    * @return Card dealt.
+    */
+   public Card dealCard() {
+      if (topCard > 0) {
+         topCard--;
+         return new Card(cards[topCard]);
+      } else {
+         return new Card('X', Card.Suit.SPADES);
+      }
+   }
+
+   public int getTopCard() {
+      return topCard;
+   }
+
+   /**
+    * Return a Card with errorFlag = true, if k is out of range.
+    * pre-condition: deck is not empty and k is within range.
+    *
+    * @param k The index of the card to inspect.
+    * @return The Card at the index or an illegal Card.
+    */
+   public Card inspectCard(int k) {
+      if (topCard != 0 && k >= 0 && k < topCard) {
+         return new Card(cards[k]);
+      }
+
+      return new Card('X', Card.Suit.DIAMONDS);
+   }
+
+   /**
+    * Fill the masterPack card array. We should only do this once,
+    * regardless of how many Deck objects there are.
+    */
+   private static void allocateMasterPack() {
+      if (masterPack[0] != null) { // return early if we have already executed this setup
+         return;
+      }
+
+      Card.Suit[] suits = Card.Suit.values();
+      char[] values = {'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'};
+
+      // fill every value of a suit before moving to the next suit
+      for (int i = 0; i < suits.length; i++) {
+         for (int j = 0; j < values.length; j++) {
+            masterPack[(values.length * i) + j] = new Card(values[j], suits[i]);
+         }
+      }
+   }
+}
+
